a442df470a673ce0cd51d6eaf3978f37cc1db100
thomasvandenberg-26 <vandenbergthomas@hotmail.fr>
2022-11-29 11:59:23 +0100

    29 nov midi

--- Web/accueil.php
+++ Web/accueil.php
@@ -4,11 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+        <link rel="header" href="header.php" />
     <title>Document</title>
 </head>
 <body>
 
-<button  id="envoi" type='submit' value='envoi' class='btn'>Envoyer </button>
+<?php 
+
+$date_inscription = [2022-11-30];
+$date_limite = [2022-12-10];
+
+if (date("Y-m-d") >= $date_inscription and $date_inscription <= $date_limite)?> <?php {  ?>
+<button type='submit' value='envoi' class='btn' onclick="location.href='formulaire_realisation';">Envoyer </button>
+}
+
+
+?>
+
+
 
     
 </body>
--- Web/formulaire_realisation.php
+++ Web/formulaire_realisation.php
@@ -9,35 +9,7 @@
         <link rel="header" href="header.php" />
     <title>Document</title>
 </head>
-<header>
-    <header>
-        <!--<h1> Gedimagination </h1>
-            <nav class="navbar navbar-inverse">
-                        <a href="accueil.php.php">Accueil</a></li>
-                        <a  href="tableau_gagnants.php">Classement </a>
-                        <a href="formulaire_realisation.php">Concours </a>
-                        <a href="login.php">Connexion </a>
-                </div>
-            </nav>-->
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#"></a>
-          </div>
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="accueil.php">Accueil</a></li>
-            <li><a  href="classement.php">Classement </a></li>
-            <li><a href="formulaire_realisation.php">Concours </a></li>
-            <li><a href="login.php">Connexion </a></li>
-            <li><a href="inscription.php">Inscription</a></li>
-            <li><a href="profil.php">profil</a></li>
-
-
-          </ul>
-        </div>
-      </nav>
-</header>
-
+  <?php include 'header.php'; ?>
 </header>
 <body>
 <?php 
@@ -63,12 +35,3 @@
 
 
 </html>
-
-<style>
-  body{
-    background-color: white;
-  }
-  p{
-    color : white; 
-  }
-</style>
